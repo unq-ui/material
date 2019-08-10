acb50cdedf4e605f0d0e03e63c07e7608a968e8a
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2019-08-10 15:22:46 -0300

    Refactoring ATM Arena example with extensions

--- ejemplos/arena/atm/src/main/java/atm/ConfirmDialog.kt
+++ ejemplos/arena/atm/src/main/java/atm/ConfirmDialog.kt
@@ -3,21 +3,23 @@ package atm
 import org.uqbar.arena.widgets.*
 import org.uqbar.arena.windows.Dialog
 import org.uqbar.arena.windows.WindowOwner
-
+import org.uqbar.arena.kotlin.extensions.*
 
 class ConfirmDialog(owner: WindowOwner, model: BankAppModel) : Dialog<BankAppModel>(owner, model) {
-
     override fun createFormPanel(mainPanel: Panel) {
-        Label(mainPanel)
-                .setText("Esta seguro que desea extraer $${amount()} de la ${accountType()}")
+        Label(mainPanel) with {
+            text = "Esta seguro que desea extraer $${amount()} de la ${accountType()}"
+        }
 
-        Button(mainPanel)
-                .setCaption("Aceptar")
-                .onClick { accept() }
+        Button(mainPanel) with {
+            caption = "Aceptar"
+            onClick { accept() }
+        }
 
-        Button(mainPanel)
-                .setCaption("Cancelar")
-                .onClick { cancel() }
+        Button(mainPanel) with {
+            caption = "Cancelar"
+            onClick { cancel() }
+        }
     }
 
     private fun amount() = modelObject.input
