b83e699f0fa11fbc9bcb39b02f441fd6840b068a
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2020-03-01 23:16:30 -0300

    :wastebasket: clean up twitter testing api

--- ejemplos/api/twittertesting/src/main/java/model/User.kt
+++
@@ -1,6 +0,0 @@
-package model
-
-class User(val username: String, var fullname: String, var password:String = "") {
-    val tweets = mutableListOf<String>()
-    fun addTweet(tweet: String) = if (tweet.isNotEmpty()) tweets.add(tweet) else ""
-}
