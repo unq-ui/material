0dd51e4ff6406a44df95b5c759be1c044e79731f
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2020-03-08 14:51:56 -0300

    :wastebasket: clean up ejemplo-clase

--- ejemplos/reactjs/ejemplo-clase/src/api/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'https://api.punkapi.com/v2'
-
-export const getBeers = () => {
-  return axios.get(`${API_URL}/beers`)
-    .then(response => response.data)
-    .catch(error => Promise.reject(error.response.data))
-}
