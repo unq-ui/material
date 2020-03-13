b9645d3b806000e5e1886d46a78e769c6779d560
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2020-03-13 20:05:13 -0300

    :wastebasket: clean up UNQ IMDB Mobile

--- ejemplos/reactNative/unq-imdb/src/components/emptyList.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { colors } from './styles/utils';
-
-const EmptyList = ({ text }) => <View><Text style={{ color: colors.pink }}>{`Not found any result of ${text}`}</Text></View>
-
-export default EmptyList;
--- ejemplos/reactNative/unq-imdb/src/resources/Storage.js
+++
@@ -1,7 +0,0 @@
-import { AsyncStorage } from 'react-native';
-
-export default {
-  setUser: (username) => AsyncStorage.setItem('userToken', username),
-  getUser: () => AsyncStorage.getItem('userToken'),
-  removeUserToken: () => AsyncStorage.removeItem('userToken'),
-};
